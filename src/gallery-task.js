55494a503c4cd07fb4f6bfeaf6513de84215bbf0
allakhimenko <all.khimenko24@gmail.com>
2021-06-23 15:47:08 +0300

    com

--- src/gallery-task.js
+++ src/gallery-task.js
@@ -1,83 +1,83 @@
-import galleryItems from "./gallery-items.js"
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
 
-const refs = {
-    gallery: document.querySelector('.js-gallery'),
-    galleryItems: document.querySelector('.gallery__item'),
-    galleryLink: document.querySelector('.gallery__link'),
-    galleryImg: document.querySelector('.gallery__image'),
-    openModal : document.querySelector ('.js-lightbox'),
-     backdrop: document.querySelector('.lightbox__overlay'),
-    modalImg : document.querySelector('.lightbox__image'),
-    modalCloseBtn : document.querySelector('.lightbox__button'),
-  }
-  
-  const galleryList = galleryItems.map(({preview,original,desciption}) => {
-    return `<li class="gallery__item">
-    <a class= "gallery__link"  href=${original}>
-      <img
-        class="gallery__image"
-        src=${preview}
-        data-source=${original}
-        alt=${desciption}
-      />
-    </a>
-  </li>`
-  }
-  ).join(' ')
-    
-  refs.gallery.insertAdjacentHTML('afterBegin', galleryList)
-  
-  const onImgClick = e => {
-    if (e.target.nodeName !== 'IMG') {
-      return
-    }
-    e.preventDefault()
-    refs.openModal.classList.add('is-open')
-    refs.modalImg.src = e.target.dataset.source
-    refs.modalCloseBtn.addEventListener('click', onCloseModal)
-    refs.backdrop.addEventListener('click', onBackDropClose)
-    window.addEventListener('keydown', onEscClose)
-    window.addEventListener('keydown',onArrowClick)
-  }
-  
-  const onCloseModal =  () => {
-    refs.openModal.classList.remove('is-open')
-    refs.modalImg.src = ""
-  }
-  
-  const onEscClose =  e => {
-    if (e.code === "Escape") {
-    onCloseModal()
-  }
-  }
-  
-  const onBackDropClose =  e => {
-    if (e.currentTarget === e.target) {
-      onCloseModal()
-    }
-  }
-  
-  const onArrowClick = e => {
-    if (e.code !== 'ArrowLeft' && e.code !== 'ArrowRight') {
-      return
-    }
-  
-    const currentImg = galleryItems.find(img => img.original === refs.modalImg.src)
-    
-    let currentIndex =
-      e.code === 'ArrowLeft'
-        ? galleryItems.indexOf(currentImg) - 1
-        : galleryItems.indexOf(currentImg) + 1;
-    if (currentIndex < 0) {
-      currentIndex = galleryItems.length -1
-    }
-    if (currentIndex === galleryItems.length) {
-      currentIndex = 0
-    }
-    
-    refs.modalImg.src = galleryItems[currentIndex].original
-    refs.modalImg.alt = galleryItems[currentIndex].description
-  }
-  
-  
-  refs.gallery.addEventListener('click', onImgClick)
+const createGalleryMarkup = galleryItems.map((item) => {
+   return `<li class="gallery__item">
+ <a
+   class="gallery__link"
+   href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+ >
+   <img
+     class="gallery__image"
+     src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+     data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+     alt="Tulips"
+   />
+ </a>
+</li>`
+  });
+
+console.log(createGalleryMarkup);
